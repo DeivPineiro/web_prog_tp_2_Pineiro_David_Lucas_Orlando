4f40279c4f488ad04dd5657ba351d12abc6a7065
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-06-04 20:53:54 -0300

    Se hace funcion personajes, resta hacer su vista.

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 const elementoCategorias = document.getElementById('divCategorias');
-const elementoBtnPersonajes = document.getElementById('btnPersonajes');
+
+const elementoPersonajes = document.getElementById('divPersonajes');
 
 
 //EJEMPLO DE COMO RECORRER UN JSON UTIL PARA LA APP
@@ -25,7 +26,7 @@ fetch(`https://swapi.dev/api/`)
     .then((resp) => {
 
 
-        console.log(resp);
+        //console.log(resp);
         elementoCategorias.innerHTML = `<p class="container text-center mt-5">Conectar estamos, tu fuerza al monitor debes...</p>`;
         return resp.json();
 
@@ -33,7 +34,7 @@ fetch(`https://swapi.dev/api/`)
     })
     .then(function (json) {
 
-        console.log(json);
+        //console.log(json);
 
         elementoCategorias.innerHTML = `<h1>Categorias</h1>`;
 
@@ -58,52 +59,58 @@ fetch(`https://swapi.dev/api/`)
 
         }
 
-
-
-
-
-
-
-
     })
 
 
 
-    //elementoBtnPersonajes.innerHTML = `<p> Cargando personajes </p>`;
-
-    fetch(`https://swapi.dev/api/people/`)
-    .then((resp) => {
-
-        console.log(resp);
-       
-        return resp.json();
-
-    })
-    .then(function (json) {
-
-        console.log(json);
+//Funcion recorrer personajes
 
 
-    })
+
+for (let i = 1; i < 10; i++) {
+
+    fetch(`https://swapi.dev/api/people/?page=${i}`)
+        .then((resp) => {
+
+            //console.log(resp);
+
+            return resp.json();
+
+        })
+        .then(function (json) {
+
+            //console.log(json.results);
+
+            json.results.forEach(element => {
+
+                cadena = JSON.stringify(element.url);
+
+                console.log(JSON.stringify(cadena));
+
+                var idPersonaje = cadena.split("/");
+
+                //console.log(idPersonaje);
+
+                elementoPersonajes.innerHTML = elementoPersonajes.innerHTML + `
+                    
+                    <div class = " col-4 "> 
+
+                    <p class="container text-center mt-5"> ${element.name} </p>
+
+                    <img class = "img-fluid" src = "img/characters/${idPersonaje[5]}.jpg">
+
+                    </div>
+                                                            
+                    `;
 
 
-    for (let i = 1; i < 10 ; i++) {
-       
-        fetch(`https://swapi.dev/api/people/?page=${i}`)
-            .then((resp) => {
 
-                console.log(resp);
+            });
 
-                return resp.json();
-               
-            })
-            .then(function (json) {
 
-                console.log(json.results);
 
-            })
+        })
 
-    }
-
+}
 
 
