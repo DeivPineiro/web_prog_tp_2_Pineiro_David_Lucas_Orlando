7c071882929765950ff8c81dae106306db2a3f12
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-06-04 21:31:15 -0300

    Se agrega seccion personajes

--- js/js.js
+++ js/js.js
@@ -1,24 +1,9 @@
 const elementoCategorias = document.getElementById('divCategorias');
 
-const elementoPersonajes = document.getElementById('divPersonajes');
 
 
-//EJEMPLO DE COMO RECORRER UN JSON UTIL PARA LA APP
 
 
-/*
-// JSON con distintos valores para utilizar en la demo
-var json = {'valor1': 1, 'valor2': [1, 2, 3, 4], 'valor3': '3'};
-     
-// Obteniendo todas las claves del JSON
-for (var clave in json){
-  // Controlando que json realmente tenga esa propiedad
-  if (json.hasOwnProperty(clave)) {
-    // Mostrando en pantalla la clave junto a su valor
-    alert("La clave es " + clave+ " y el valor es " + json[clave]);
-  }
-}
-*/
 
 elementoCategorias.innerHTML = `<div class="container text-center mt-5"><p>Si esperar tu puedes, categorias ver pronto...</p></div>`;
 
@@ -34,7 +19,7 @@ fetch(`https://swapi.dev/api/`)
     })
     .then(function (json) {
 
-        //console.log(json);
+        console.log(json);
 
         elementoCategorias.innerHTML = `<h1>Categorias</h1>`;
 
@@ -48,7 +33,7 @@ fetch(`https://swapi.dev/api/`)
                            
                 <div class="col-4 rounded">
                 
-                <img class="img-fluid rounded border shadow border-dark" src="img/categories/${i}.jpg" alt="categoria">
+                <a href = "${i}.html"><img class="img-fluid rounded border shadow border-dark" src="img/categories/${i}.jpg" alt="categoria"></a>
                 <p class="text-center">${i}</p>
                 
                 </div>           
@@ -62,9 +47,10 @@ fetch(`https://swapi.dev/api/`)
     })
 
 
-
-//Funcion recorrer personajes
-
+/////////////////////////////////
+//Funcion recorrer personajes///
+////////////////////////////////
+/*
 
 
 for (let i = 1; i < 10; i++) {
@@ -85,7 +71,7 @@ for (let i = 1; i < 10; i++) {
 
                 cadena = JSON.stringify(element.url);
 
-                console.log(JSON.stringify(cadena));
+                //console.log(JSON.stringify(cadena));
 
                 var idPersonaje = cadena.split("/");
 
@@ -111,6 +97,59 @@ for (let i = 1; i < 10; i++) {
 
         })
 
+}*/
+
+//////////////////////
+// Seccion Planetas//
+////////////////////
+/*
+for (let i = 1; i < 8; i++) {
+
+    fetch(`https://swapi.dev/api/planets/?page=${i}`)
+        .then((resp) => {
+
+            //console.log(resp);
+
+            return resp.json();
+
+        })
+        .then(function (json) {
+
+            //console.log(json.results);
+
+            json.results.forEach(element => {
+
+                cadena = JSON.stringify(element.url);
+
+                //console.log(JSON.stringify(cadena));
+
+                var id = cadena.split("/");
+
+                //console.log(idPersonaje);
+
+                elementoPersonajes.innerHTML = elementoPersonajes.innerHTML + `
+                    
+                    <div class = " col-4 "> 
+
+                    <p class="container text-center mt-5"> ${element.name} </p>
+
+                    <img class = "img-fluid" src = "img/planets/${id[5]}.jpg">
+
+                    
+                    </div>
+                                                            
+                    `;
+
+
+
+            });
+
+
+
+        })
+
 }
+
+*/
 
 
