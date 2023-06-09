97d8eb76048d822d97fa9da3065c4b3a23075090
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-06-09 19:43:36 -0300

    Se modifica y arregla lo de pesonaje segun profe

--- js/elemento.js
+++ js/elemento.js
@@ -16,41 +16,41 @@ if (tipo == 1) {
 
 
 
-    fetch(`https://swapi.dev/api/people/${data}`)
-        .then((resp) => {
+  fetch(`https://swapi.dev/api/people/${data}`)
+    .then((resp) => {
 
-            //console.log(resp);
+      //console.log(resp);
 
-            return resp.json();
+      return resp.json();
+
+    })
+    .then(function (json) {
+
+      // console.log(json.name);
+
+      cadena = JSON.stringify(json.homeworld)
+      var idPlaneta = cadena.split("/")
+      console.log(idPlaneta)
+
+      fetch(`${json.homeworld}`)
+        .then((respuesta) => {
+
+
+          return respuesta.json();
 
         })
-        .then(function (json) {
+        .then(function (j) {
 
-           // console.log(json.name);
-
-            cadena = JSON.stringify(json.homeworld)
-            var idPlaneta = cadena.split("/")
-            console.log(idPlaneta)
-
-            fetch(`${json.homeworld}`)
-                .then((respuesta) => {
-
-                   
-                    return respuesta.json();
-
-                })
-                .then(function (json) {
-                   
-                    planeta = JSON.stringify(json.name);
-                    
-                    
-                    localStorage.setItem("planeta", planeta)
-                    
-                    
-                })
+          planeta = JSON.stringify(j.name);
 
 
-            elementoElemento.innerHTML = `
+          localStorage.setItem("planeta", planeta)
+
+
+
+
+
+          elementoElemento.innerHTML = `
         
         <h1 class="bg-dark text-center text-light">${json.name}</h1>
 
@@ -70,7 +70,7 @@ if (tipo == 1) {
     <li class="list-group-item">Color piel: ${json.skin_color}</li>
     <li class="list-group-item">Color de ojos: ${json.eye_color}</li>
     <li class="list-group-item">Genero: ${json.gender}</li>
-    <li class="list-group-item">Planeta origen: ${localStorage.getItem("planeta")}</li>
+    <li class="list-group-item">Planeta origen: ${j.name}</li>
     <li class="list-group-item"><img class="img-fluid d-block m-auto border border-warning shadow" style="max-width:110px" src="img/planets/${idPlaneta[5]}.jpg" alt="planeta"></li>
 
     
@@ -85,8 +85,8 @@ if (tipo == 1) {
         
         
         `;
-
         })
+    })
 
 
 
